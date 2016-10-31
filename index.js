e0457bc545a828739deca32a58f9b31a1148b90d
eugene_karmyzau <jauhenkarmyzau@gmail.com>
2016-10-31 01:14:07 +0000

    WS Server added

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const http = require('http');
 
 // Start the HTTP server using express
 const app = express();
+const PORT = process.env.PORT || 8080;
 const httpServer = http.createServer(app);
-httpServer.listen(process.env.PORT);
-console.log('Started HTTP Server on ' + process.env.PORT + ' port');
+httpServer.listen(PORT);
+console.log('Started HTTP Server on ' + PORT + ' port');
+
+// Start WS server for desktop
+const WS_PORT = 8081;
+const wsServer = http.createServer(function (req, res) {
+    console.log('http reques to WS server');
+    res.writeHead(404);
+    res.end();
+});
+wsServer.listen(WS_PORT);
+console.log('Started WS HTTP Server on ' + WS_PORT + ' port');
 
 // Setup routes
 var router = require('./server/router');
